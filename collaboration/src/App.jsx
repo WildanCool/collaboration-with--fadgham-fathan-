d4f88dcffc1ac6ffb11160fd0b94291906e0471e
Tzuwiing <jusuf1909@student.abudzar.sch.id>
2025-05-02 11:15:08 +0700

    jiwo

--- collaboration/src/App.jsx
+++ collaboration/src/App.jsx
@@ -1,10 +1,14 @@
 import potoWildan from "./assets/PotoWildan.jpg";
+import potoFathan from "./assets/ancur.jpg";
 
 export default function App() {
   return (
     <>
       <h1 className="text-3xl font-bold">Ini Project -Wildan</h1>
       <img src={potoWildan} alt="" className="w-1/2" />
+
+      <h1 className="text-3xl font-bold">Ini Project -Fathan</h1>
+      <img src={potoFathan} alt="" className="w-1/2" />
     </>
   );
 }

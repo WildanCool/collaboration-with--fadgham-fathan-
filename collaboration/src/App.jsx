fb5bea9a32eca0da3118be2ede2bd9b817c8c97a
WildanCool <wildan2512@student.abudzar.sch.id>
2025-05-02 11:04:47 +0700

    Update App.jsx

--- collaboration/src/App.jsx
+++ collaboration/src/App.jsx
@@ -3,9 +3,7 @@ import potoWildan from "./assets/PotoWildan.jpg";
 export default function App() {
   return (
     <>
-      <h1 className="text-3xl font-bold underline text-red-500">
-        Ini Project -Wildan
-      </h1>
+      <h1 className="text-3xl font-bold">Ini Project -Wildan</h1>
       <img src={potoWildan} alt="" className="w-1/2" />
     </>
   );
